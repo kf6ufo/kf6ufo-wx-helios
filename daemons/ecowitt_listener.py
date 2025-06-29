d4b42dc5146cbd905827a216989d31e8e76033c0
KF6UFO <jerry.joplin+kf0tke@gmail.com>
2025-06-28 19:03:25 -0600

    Remove experimental WX fields

--- daemons/ecowitt_listener.py
+++ daemons/ecowitt_listener.py
@@ -117,7 +117,7 @@ def ecowitt_to_aprs(p):
         f"{t_field}"
         f"r{rRRR:03d}p{pPPP:03d}P{PQQQ:03d}"
         f"h{rh:02d}b{bp:05d}"
-        f"X{_version}WxHe 1 WX Helios by KF6UFO"
+        f" WX Helios by KF6UFO"
     )
 
 
